6fda66f74d4bfaccaf52b230b598266aecc5d79e
Robin Moisson <robin.moisson@gmail.com>
2016-06-09 14:48:23 +0800

    new js file for bootstrap theme

---
+++ Resources/public/js/select2entity-bootstrap.js
@@ -0,0 +1,19 @@
+$(document).ready(function () {
+    $.fn.select2entity = function(action) {
+        var action = action || {};
+        this.select2($.extend(action, {
+            theme: 'bootstrap',
+            ajax: {
+                data: function (params) {
+                    return {
+                        q: params.term,
+                        page: params.page || 1
+                    };
+                }
+            }
+        }));
+        return this;
+    };
+
+    $('.select2entity').select2entity();
+});
